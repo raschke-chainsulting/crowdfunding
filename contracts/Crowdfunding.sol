cbe28816e3394498d55d27aa5827a2d33ca45ecd
Lars Raschke <l.raschke@chainsulting.de>
2022-12-12 18:07:08 +0100

    Add: Deadline and withdrawn flag as project vars - owner not able to withdraw before deadline - flag indicating previous withdrawl - participate ampount must be exact - enhance documentation

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -10,7 +10,7 @@ import {ICrowdfunding} from "./ICrowdfunding.sol";
  * @dev Complete source code: https://github.com/raschke-chainsulting/crowdfunding
  * @notice This contract implements a crowdfunding platform. It allows users to
  * create crowdfunding projects and participate to them. The owner of a project
- * can withdraw the funds of the project.
+ * can withdraw the funds of the project when a defined deadline is reached.
  */
 contract Crowdfunding is ICrowdfunding {
     // counter to generate unique project ids
@@ -49,11 +49,18 @@ contract Crowdfunding is ICrowdfunding {
      * @param _participationAmount The amount of ether that must be sent to the
      * project to participate to it.
      * @return The id of the created project.
+     *
+     * @custom:requirements
+     * - The title must not be empty.
+     * - The description must not be empty.
+     * - The participation amount must be greater than 0.
+     * - The deadline must be in the future.
      */
     function createProject(
         string calldata _title,
         string calldata _description,
-        uint256 _participationAmount
+        uint256 _participationAmount,
+        uint256 _deadline
     ) external override returns (uint256) {
         // check if input parameters are valid
         require(bytes(_title).length > 0, "Title must not be empty");
@@ -65,21 +72,24 @@ contract Crowdfunding is ICrowdfunding {
             _participationAmount > 0,
             "Participation amount must be greater than 0"
         );
+        require(_deadline > block.timestamp, "Deadline must be in the future");
 
         // create new project and push it to the projects array
         projects.push(
             Project(
-                projectIdCounter,
-                _title,
-                _description,
-                payable(msg.sender),
-                _participationAmount,
-                0
+                projectIdCounter, // id of the project
+                _title, // title of the project
+                _description, // description of the project
+                payable(msg.sender), // owner of the project
+                _participationAmount, // participation amount of the project
+                0, // total collected funding amount of the project
+                _deadline, // deadline of the project
+                false // if the owner of the project has withdrawn the funds
             )
         );
 
         // emit event for the created project
-        emit ProjectCreated(projectIdCounter, msg.sender);
+        emit ProjectCreated(projectIdCounter, msg.sender, _deadline);
 
         // increment project id counter
         projectIdCounter++;
@@ -92,16 +102,28 @@ contract Crowdfunding is ICrowdfunding {
      * @notice The caller of this function will participate to the project with
      * the given id. The amount of ether sent with the transaction will be
      * added to the project's balance if it is not already finished. The amount
-     * of ther must be the defined participation amount.
+     * of ether must be the defined participation amount. Users can participate
+     * to a project multiple times.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
+     * - The amount of ether sent with the transaction must be the defined
+     *  participation amount.
+     * - The deadline of the project must not have passed yet.
      */
     function participateToProject(
         uint256 _projectId
     ) external payable override projectExists(_projectId) {
         // check if participation amount is correct
         require(
-            msg.value >= projects[_projectId].participationAmount,
-            "Not enough funds sent"
+            msg.value == projects[_projectId].participationAmount,
+            "Participation amount is incorrect"
         );
+        require(
+            block.timestamp < projects[_projectId].deadline,
+            "Deadline has passed"
+        );
+
         // update users contributions
         contributions[msg.sender][_projectId] += msg.value;
 
@@ -113,9 +135,56 @@ contract Crowdfunding is ICrowdfunding {
     }
 
     /**
-     * @notice Returns the project with the given id.
+     * @notice Withdraws the funds of the project with the given id. The caller
+     * must be the owner of the project. The withdrawl flag will be set by calling
+     * this function allowing the owner to withdraw the funds only once.
      * @param _projectId The id of the project.
-     * @return The project with the given id.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
+     * - The caller of this function must be the owner of the project.
+     * - The deadline of the project must have passed.
+     * - The project must have funds to withdraw.
+     * - The funds must not have been withdrawn yet.
+     */
+    function withdrawlFunds(
+        uint256 _projectId
+    ) external override projectExists(_projectId) onlyProjectOwner(_projectId) {
+        // check if withdrawl has already been done
+        require(
+            !projects[_projectId].withdrawn,
+            "Funds have already been withdrawn"
+        );
+        // check if the dead line has passed
+        require(
+            block.timestamp > projects[_projectId].deadline,
+            "Deadline has not passed yet"
+        );
+        // check if there are funds to withdraw
+        require(
+            projects[_projectId].totalFundingAmount > 0,
+            "No funds to withdraw"
+        );
+
+        // set withdrawn flag to true to prevent multiple withdrawls
+        projects[_projectId].withdrawn = true;
+        // emit withdrawl event
+        emit FundsWithdrawn(
+            _projectId,
+            msg.sender,
+            projects[_projectId].totalFundingAmount
+        );
+        // transfer the amount to the owner
+        payable(msg.sender).transfer(projects[_projectId].totalFundingAmount);
+    }
+
+    /**
+     * @notice Returns the project data with the given id.
+     * @param _projectId The id of the project.
+     * @return The project data for the given id.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
      */
     function searchForProject(
         uint256 _projectId
@@ -136,35 +205,14 @@ contract Crowdfunding is ICrowdfunding {
      * @param _projectId The id of the project.
      * @return The amount of ether contributed by the given contributor to the
      * project with the given id.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
      */
     function retrieveContributions(
         address _contributor,
         uint256 _projectId
     ) external view override projectExists(_projectId) returns (uint256) {
         return contributions[_contributor][_projectId];
-    }
-
-    /**
-     * @notice Withdraws the funds of the project with the given id. The caller
-     * must be the owner of the project.
-     * @param _projectId The id of the project.
-     */
-    function withdrawlFunds(
-        uint256 _projectId
-    ) external override projectExists(_projectId) onlyProjectOwner(_projectId) {
-        // check if there are funds to withdraw
-        require(
-            projects[_projectId].totalFundingAmount > 0,
-            "No funds to withdraw"
-        );
-
-        // get the amount to withdraw
-        uint256 amount = projects[_projectId].totalFundingAmount;
-        // reset total funding amount
-        projects[_projectId].totalFundingAmount = 0;
-        // emit withdrawl event
-        emit FundsWithdrawn(_projectId, msg.sender, amount);
-        // transfer the amount to the owner
-        payable(msg.sender).transfer(amount);
     }
 }
--- contracts/ICrowdfunding.sol
+++ contracts/ICrowdfunding.sol
@@ -12,22 +12,26 @@ pragma solidity 0.8.7;
 interface ICrowdfunding {
     // The struct that defines a crowdfunding project.
     struct Project {
-        uint256 id;
-        string title;
-        string description;
-        address payable owner;
-        uint256 participationAmount;
-        uint256 totalFundingAmount;
+        uint256 id; // unique identifier of the project
+        string title; // title of the project
+        string description; // description of the project
+        address payable owner; // address of the projects owner
+        uint256 participationAmount; // exact amount of ether that must be sent to participate to the project
+        uint256 totalFundingAmount; // total amount of ether contributed to the project
+        uint256 deadline; // timestamp of the projects deadline as unix timestamp in seconds
+        bool withdrawn; // true if the owner of the project has already withdrawn the funds
     }
 
     /**
      * @notice Emitted when a new project is created.
      * @param _projectId The id of the created project.
      * @param _projectOwner The address of the project owner.
+     * @param _deadline The timestamp of the deadline of the project.
      */
     event ProjectCreated(
         uint256 indexed _projectId,
-        address indexed _projectOwner
+        address indexed _projectOwner,
+        uint256 _deadline
     );
 
     /**
@@ -62,20 +66,49 @@ interface ICrowdfunding {
      * @param _participationAmount The amount of ether that must be sent to the
      * project to participate to it.
      * @return The id of the created project.
+     *
+     * @custom:requirements
+     * - The title must not be empty.
+     * - The description must not be empty.
+     * - The participation amount must be greater than 0.
+     * - The deadline must be in the future.
      */
     function createProject(
         string calldata _title,
         string calldata _description,
-        uint256 _participationAmount
+        uint256 _participationAmount,
+        uint256 _deadline
     ) external returns (uint256);
 
     /**
      * @notice The caller of this function will participate to the project with
      * the given id. The amount of ether sent with the transaction will be
      * added to the project's balance if it is not already finished. The amount
-     * of ther must be the defined participation amount.
+     * of ether must be the defined participation amount. Users can participate
+     * to a project multiple times.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
+     * - The amount of ether sent with the transaction must be the defined
+     *  participation amount.
+     * - The deadline of the project must not have passed yet.
      */
     function participateToProject(uint256 _projectId) external payable;
+
+    /**
+     * @notice Withdraws the funds of the project with the given id. The caller
+     * must be the owner of the project. The withdrawl flag will be set by calling
+     * this function allowing the owner to withdraw the funds only once.
+     * @param _projectId The id of the project.
+     *
+     * @custom:requirements
+     * - The project with the given id must exist.
+     * - The caller of this function must be the owner of the project.
+     * - The deadline of the project must have passed.
+     * - The project must have funds to withdraw.
+     * - The funds must not have been withdrawn yet.
+     */
+    function withdrawlFunds(uint256 _projectId) external;
 
     /**
      * @notice Returns the project with the given id.
@@ -98,11 +131,4 @@ interface ICrowdfunding {
         address _contributor,
         uint256 _projectId
     ) external view returns (uint256);
-
-    /**
-     * @notice Withdraws the funds of the project with the given id. The caller
-     * must be the owner of the project.
-     * @param _projectId The id of the project.
-     */
-    function withdrawlFunds(uint256 _projectId) external;
 }
